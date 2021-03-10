fb5c39ec2af636799ebf72327242336e1fc28266
Pooja <shettypooja2000@gmail.com>
2021-03-10 17:38:23 +0530

    Initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Aux from "../../components/hoc/Aux1";
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        )
+
+    }
+}
+
+export default BurgerBuilder
