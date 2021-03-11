70db9ed4134a492632763bbb20a5445359144797
Pooja <shettypooja2000@gmail.com>
2021-03-11 10:01:46 +0530

    Burger Component is created and a burger is displayed on screen

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,11 @@
 import React, {Component} from "react";
 import Aux from "../../components/hoc/Aux1";
+import Burger from "../../components/Burger/Burger"
 class BurgerBuilder extends Component{
     render(){
         return(
             <Aux>
-                <div>Burger</div>
-                <div>Build Controls</div>
+                <Burger/>
             </Aux>
         )
 
