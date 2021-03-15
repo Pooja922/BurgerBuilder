a2d0a1c57c944cff9b36e7f96102e0bdb7abf26c
Pooja <shettypooja2000@gmail.com>
2021-03-15 14:04:01 +0530

    Handling errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,8 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls"
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axios-orders';
-import Spinner from "../../components/UI/Spinner/Spinner"
+import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../components/hoc/withErrorHandler/withErrorHandler"
 
 const INGREDIENT_PRICES={
     salad:0.5,
@@ -134,4 +135,4 @@ class BurgerBuilder extends Component{
     }
 }
 
-export default BurgerBuilder
+export default withErrorHandler(BurgerBuilder,axios)
