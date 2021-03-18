fcd1428ed0c83929c029c43997d155b886306f69
Pooja <shettypooja2000@gmail.com>
2021-03-18 15:53:58 +0530

    Initializing Ingredients in the

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -35,6 +35,17 @@ const burgerBuilder=(state=initialState, action)=>{
                 totalPrice: state.totalPrice-INGREDIENT_PRICES[action.ingredientName]
             }
         }
+        case actionTypes.SET_INGREDIENTS:
+            return{
+                ...state,
+                ingredients:action.ingredients,
+                error:false
+            }
+        case actionTypes.FETCH_INGREDIENTS_FAILED:
+            return{
+                ...state,
+                error:true
+            }
         default:
             return state
     }
