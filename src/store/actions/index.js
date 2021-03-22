44a865d5e50794deae99455b99c91df7a9b51954
Pooja <shettypooja2000@gmail.com>
2021-03-22 16:25:05 +0530

    Adding a logout link

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export{addIngredient,removeIngredient,initIngredients} from './burgerBuilder'
 export { purchaseBurger,purchaseInit,fetchOrders,purchaseBurgerSuccess,purchaseBurgerStart,purchaseBurgerFail } from "./order"
-export { auth } from "./auth"
+export { auth,logout } from "./auth"
